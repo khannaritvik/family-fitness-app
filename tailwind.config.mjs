7f98e10c9ef7009fa50dce4982c4ba17f96039ee
Ritvik Khanna <ritvik@ualberta.ca>
2025-09-14 23:22:47 -0600

    chore: ESM configs for Tailwind/PostCSS + normalize eol

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: { extend: {} },
-  plugins: []
+  plugins: [],
 };
